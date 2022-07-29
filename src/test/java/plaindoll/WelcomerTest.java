8cdb7284f795e03ab99e666af924e75ccb9f4fdf
Evgeniy Smirnov <evgenplay@inbox.ru>
2022-07-29 12:55:37 +0300

    hunter

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -26,4 +26,8 @@ public class WelcomerTest {
 	public void welcomerSaysSilver(){
 		assertThat(welcomer.sayNeedGold(), containsString("gold"));
 	}
+	@Test
+	public void welcomerSaysNeedHW(){
+		assertThat(welcomer.sayNeedHW(), containsString("hunter"));
+	}
 }
